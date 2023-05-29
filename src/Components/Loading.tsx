cae5d5763bfb03855b8a998b2b21f68015321823
Elias Galindo <eliasgalindo@gmail.com>
2023-05-29 16:31:23 -0300

    Fix: Loading position

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -3,8 +3,8 @@ export default function Loading(): JSX.Element {
     <section className="loading">
       {
         <>
-          <img src="https://portal.ufvjm.edu.br/a-universidade/cursos/grade_curricular_ckan/loading.gif/loading.gif" alt="loading" width="50px" />
           <p>Carregando...</p>
+          <img src="https://portal.ufvjm.edu.br/a-universidade/cursos/grade_curricular_ckan/loading.gif/loading.gif" alt="loading" width="50px" />
         </>
 
       }
